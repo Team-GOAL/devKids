6a9ad56dfabe8c39dcf5f8b7fbfaaf4f2f9bb2e0
Gabe <gnai0001@student.monash.edu>
2019-09-17 09:59:05 +1000

    Add existing file

---
+++ php/index.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>MuziVic</title>
+    <meta name="description" content="">
+    <meta name="robots" content="noindex, follow"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon chang it!!-->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+
+    <!-- all css here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icons.css">
+    <link rel="stylesheet" href="assets/css/plugins.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <!--fonts added here-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair%20Display">
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+    <script type="text/javascript">WebFont.load({google: {families: ["Karla:regular,italic,700", "Frank Ruhl Libre:300,regular", "Playfair Display:regular,italic,700,700italic,900,900italic"]}});</script>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Karla:regular,italic,700%7CFrank+Ruhl+Libre:300,regular%7CPlayfair+Display:regular,italic,700,700italic,900,900italic"
+          media="all">
+    <link href="https://fonts.googleapis.com/css?family=Lora&display=swap" rel="stylesheet">
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+
+</head>
+<body class="wrapper">
+<!-- header start -->
+<?php include "header.html" ?>
+<!-- Slider Start -->
+<div class="slider-area">
+        <div class="single-slider bg-img pt-50 slider-height-2 d-flex align-items-center"
+             style="background-image: url(assets/img/kidStudying.jpeg);">
+            <div class="container">
+                <div class="row">
+                    <div class="ml-auto col-lg-4 col-md-6">
+                        <div class="slider-text-6 slider-animated-1">
+                            <h1 class="animated">Raise Confident Kids</h1>
+                            <h3 class="animated">
+                                Supporting Chinese school kids in Victoria
+                            </h3>
+                            <div class="slider-btn-4 mt-30">
+                                <a class="default-btn animated btn-hover" href="single-product.html">See Our Mission</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+</div>
+<br>
+<div class="summer-addition-area pb-115">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-md-5">
+                <div class="successful-img">
+                    <img alt="" src="assets/img/boy%20plays%20baseball.jpeg">
+                </div>
+            </div>
+            <div class="col-lg-1 col-md-1"></div>
+            <div class="col-lg-6 col-md-6">
+                <div class="successful-content">
+                    <h2>Kids' sports for a healthy life and stronger mind</h2>
+                    <p>Playing sports of similar interest with others will increase your child's sense of belonging to
+                        the
+                        greater Australian community.</p>
+                    <div class="slider-btn-4 mt-30">
+                        <a class="default-btn animated btn-hover" href="single-product.html">Learn More</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="summer-addition-area pb-115">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-1 col-md-1"></div>
+            <div class="col-lg-6 col-md-6">
+                <div class="successful-content">
+                    <h2>Support your kid in flish and Communication</h2>
+                    <p>Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh
+                        id elit. Duis sed odio sit amet nibh vulpu tate cursus a sit amet mauris accumsan velit.</p>
+                    <div class="slider-btn-4 mt-30">
+                        <a class="default-btn animated btn-hover" href="single-product.html">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5 col-md-5">
+                <div class="successful-img">
+                    <img alt="" src="assets/img/kids%20play%20together.jpeg">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="summer-addition-area pb-115">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-md-5">
+                <div class="successful-img">
+                    <img alt="" src="assets/img/kid-ipad.jpeg">
+                </div>
+            </div>
+            <div class="col-lg-1 col-md-1"></div>
+            <div class="col-lg-6 col-md-6">
+                <div class="successful-content">
+                    <h2>Raise an independent kid at solving problems</h2>
+                    <p>Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh
+                        id elit. Duis sed odio sit amet nibh vulpu tate cursus a sit amet mauris accumsan velit.</p>
+                    <div class="slider-btn-4 mt-30">
+                        <a class="default-btn animated btn-hover" href="single-product.html">Learn More</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+</div>
+
+<?php include "footer.html" ?>
+</body>
+<!-- all js here -->
+
+<script src="assets/js/vendor/jquery-1.12.0.min.js"></script>
+<script src="assets/js/popper.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/ajax-mail.js"></script>
+<script src="assets/js/plugins.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
